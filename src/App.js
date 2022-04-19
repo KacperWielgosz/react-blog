318499e897bb38d50028fd344c57e808d2313865
K W <k4cperwi3lgosz@gmail.com>
2022-04-19 14:38:30 +0200

    Create basic structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap'
+
+import About from './components/pages/About';
+import Edit from './components/pages/post/Edit';
+import Add from './components/pages/post/Add';
+import Post from './components/pages//post/Post';
+import NoMatch from './components/pages/NoMatch';
+import Home from './components/pages/Home';
+import Footer from './components/views/Footer';
+import Header from './components/views/Header';
+import NavBar from './components/views/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <NavBar />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/post/edit/:id" element={<Edit />} />
+        <Route path="/post/add" element={<Add />} />
+        <Route path="/post/:id" element={<Post />} />
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+      <Footer />
+    </Container>
   );
 }
 
---
+++ src/redux/initialState.js
@@ -0,0 +1,30 @@
+const initialState = {
+  posts: [
+    {
+      id: '1',
+      title: 'Article title 1',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: '02-02-2022',
+      author: 'John Doe'
+    },
+    {
+      id: '2',
+      title: 'Article title 2',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: '03-01-2022',
+      author: 'John Doea'
+    },
+    {
+      id: '3',
+      title: 'Article title 3',
+      shortDescription: 'Short description of the article...',
+      content: 'Main content of the article',
+      publishedDate: '04-06-2022',
+      author: 'John Doer'
+    },
+  ],
+}
+
+export default initialState
